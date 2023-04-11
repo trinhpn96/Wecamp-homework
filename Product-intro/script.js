02e6faa3fbb500a4ba51fd19dce152f9f5f321c2
Ngọc Trinh Phạm <trinhpn96@gmail.com>
2023-04-11 23:13:29 +0700

    initial commit

---
+++ Product-intro/script.js
@@ -0,0 +1,20 @@
+let slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+  showDivs((slideIndex += n));
+}
+
+function showDivs(n) {
+  let x = document.getElementsByClassName("shoes");
+  if (n > x.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = x.length;
+  }
+  for (let i = 0; i < x.length; i++) {
+    x[i].style.display = "block";
+  }
+  x[slideIndex - 1].style.display = "block";
+}
